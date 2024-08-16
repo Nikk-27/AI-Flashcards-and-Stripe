c152404cef71a9eb289339f9010bff8b0b117af6
Jad Alriyabi <71855107+Jadalriyabi@users.noreply.github.com>
2024-08-16 15:05:23 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-import { Container, AppBar, Toolbar, Typography, Button} from '@mui/material';
+import { Container, AppBar, Toolbar, Typography, Button, Box, Grid} from '@mui/material';
 import Head from 'next/head';
 
 export default function Home() {
@@ -13,16 +13,68 @@ export default function Home() {
           </Head>
           <AppBar position="static">
               <Toolbar>
-                  <Typography variant="h6">Flashcard SaaS</Typography>
+                  <Typography variant="h6" style={{flexGrow:1}}>
+                    Flashcard SaaS</Typography>
                   <SignedOut>
-                    <Button> Login</Button>
-                    <Button> Sign Up</Button>
+                    <Button color="inherit"> Login</Button>
+                    <Button color="inherit"> Sign Up</Button>
                   </SignedOut>
                   <SignedIn>
                     <UserButton></UserButton>
                   </SignedIn>
               </Toolbar>
           </AppBar>
+          
+          <Box
+            sx={{
+              textAlign: 'center',
+              my: 4,
+
+            }}>
+            <Typography variant ="h2"> Welcome to Flashcard SaaS</Typography>
+            <Typography variant ="h6"> 
+              {' '}
+              The easiest way to make flashcards from your text
+            </Typography>
+            <Button variant="contained" color ="primary" sx={{mt:2}}>
+              Get Started
+            </Button>
+            </Box>
+            <Box sx = {{my: 6}}>
+              <Typography variant="h4" components= "h2">
+                Features
+              </Typography>
+              <Grid contained spacing={4}>
+                <Grid items xs={12} md={4}>
+                  <Typography variant="h6">
+                    Easy Text Input
+                  </Typography>
+                  <Typography>
+                    {''}
+                    Simply input your text and let our software do the rest. Creating flashcards has never been easier
+                  </Typography>
+                </Grid>
+                <Grid items xs={12} md={4}>
+                  <Typography variant="h6">
+                    Easy Text Input
+                  </Typography>
+                  <Typography>
+                    {''}
+                    Simply input your text and let our software do the rest. Creating flashcards has never been easier
+                  </Typography>
+                </Grid>
+                <Grid items xs={12} md={4}>
+                  <Typography variant="h6">
+                    Easy Text Input
+                  </Typography>
+                  <Typography>
+                    {''}
+                    Simply input your text and let our software do the rest. Creating flashcards has never been easier
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Box>
+
       </Container>
   );
 }
