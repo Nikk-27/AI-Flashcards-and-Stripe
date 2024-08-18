c75c5233b048e8bccd726309f9e9e51ec2f34e27
Nikk-27 <nktllwn@gmail.com>
2024-08-17 22:12:37 -0700

    3rd part done

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
@@ -6,6 +8,28 @@ import Head from 'next/head';
 import { School, FlashOn, Devices } from '@mui/icons-material';
 
 export default function Home() {
+  const handleSubmit = async () => {
+    const checkoutSession = await fetch('/api/checkout_session', {
+    method: 'POST',
+    headers: {
+      origin: 'http://localhost:3000',
+    },
+    })
+    const checkoutSessionJson = await checkoutSession.json()
+      if (checkoutSession.statusCode === 500) {
+        console.error(checkoutSession.message)
+        return
+      }
+
+      const stripe = await getStripe()
+      const {error} = await stripe.redirectToCheckout({
+        sessionId : checkoutSessionJson.id
+      })
+
+      if (error) {
+        console.warn(error.message)
+      }
+  }
   return (
     <Container maxWidth="100vw">
       <Head>
@@ -119,7 +143,7 @@ export default function Home() {
               <Typography variant="h6" gutterBottom>
                 Unlimited flashcards and storage, with priority support.
               </Typography>
-              <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+              <Button variant="contained" color="primary" sx={{ mt: 2 }} onClick={handleSubmit}>
                 Choose Pro
               </Button>
             </Box>
